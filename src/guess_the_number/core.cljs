44db9969133db415fe53a1ff4162b3a47fd19113
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2016-01-17 19:19:31 +0100

    Add input field for guessing

--- src/guess_the_number/core.cljs
+++ src/guess_the_number/core.cljs
@@ -6,9 +6,20 @@
 
 (def container (.getElementById js/document "main"))
 
+(defn enter-key? [evt]
+  (= 13 (.-keyCode evt)))
+
+(q/defcomponent Guess []
+  (dom/input {
+       :placeholder "Guess and press enter"
+       :onKeyDown (fn [event]
+                    (when (enter-key? event)
+                      (println (.-value (.-target event)))))}))
+
 (q/defcomponent Game []
   (dom/div {}
-           (dom/p {} "Which number between 1 and 30?")))
+           (dom/p {} "Which number between 1 and 30?")
+           (Guess)))
 
 (defn render-game [container]
   (q/render (Game) container))
