f3e6a71ad10611565c79fd6a297ddf779056b672
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2016-01-17 19:30:00 +0100

    Tell the user if guess is correct, otherwise show a hint (low/high)

--- src/guess_the_number/core.cljs
+++ src/guess_the_number/core.cljs
@@ -8,7 +8,6 @@
                     {:number (rand-int 31)}))
 
 (defn make-guess [guess]
-  (println (str "Guessing: " guess))
   (swap! app-state (fn [state]
                      {:number (:number state)
                       :guess guess})))
@@ -25,10 +24,18 @@
                     (when (enter-key? event)
                       (make-guess (int (.-value (.-target event))))))}))
 
+(q/defcomponent Hint [{:keys [number guess]}]
+  (dom/p {}
+         (when (< guess number) "Too low")
+         (when (> guess number) "Too high")
+         (when (== guess number) "Correct!")))
+
 (q/defcomponent Game [state]
   (dom/div {}
            (dom/p {} "Which number between 1 and 30?")
-           (Guess)))
+           (Guess)
+           (when (:guess state)
+             (Hint state))))
 
 (defn render-game [container state]
   (q/render (Game state) container))
